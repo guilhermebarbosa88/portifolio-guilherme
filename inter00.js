b0885b856a0825f84aa5784db61f2b4b0ce1d1a5
Guilherme Felix Barbosa <guilhermebarbosa8846@yahoo.com>
2026-07-08 10:48:54 -0300

    js

--- inter00.js
+++ inter00.js
@@ -6,3 +6,9 @@ function toggleMenu() {
     nav.classList.toggle('active');
 };
 btnMobile.addEventListener('click', toggleMenu);
+
+// Mudar de idioma
+function changeLanguage() {
+    const language = document.getElementById('language-select').value;
+    // Lógica para mudar o idioma
+}
